4fc708514e3dc3620862424ac1aa2a2567112a61
Kumarramaresh@05 <kumarramaresh@gmail.com>
2020-04-29 11:59:14 +0530

    five commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,12 +3,15 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TaskManager', { useNewUrlParser: true }).then(() => {
+//mongodb://localhost:27017/TaskManager
+//mongodb+srv://amarDB:Amardb@mydb-rabpp.mongodb.net/test?retryWrites=true&w=majority
+mongoose.connect('mongodb+srv://amarDB:Amardb@mydb-rabpp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => {
     console.log("Connected to MongoDB successfully :)");
 }).catch((e) => {
     console.log("Error while attempting to connect to MongoDB");
     console.log(e);
 });
+
 
 // To prevent deprectation warnings (from MongoDB native driver)
 mongoose.set('useCreateIndex', true);
